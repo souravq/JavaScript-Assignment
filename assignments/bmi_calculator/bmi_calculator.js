1ac96ca0c1fca27781cb4858ed363db2ceb82ad8
souravq <souravbera515@gmail.com>
2022-01-17 15:56:17 +0530

    Final Commit BMI Calculator

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,6 +1,13 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
     // Write your code here
+    if (mass>0 && height>0){
+        var BMI = mass / (height * height);
+    }else{
+        //console.log("INVALID INPUT");
+        return "INVALID INPUT";
+    }
+    return BMI;
 }
 
 module.exports = BMICalculator;
